cb6d622e29a5504d704f4f641f2979700aca6c4b
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-04-08 13:42:30 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,20 @@ public class PracticeProblem {
 
 	}
 
+	public static void insertionSort(char[] characters) {
+		int n = characters.length;
+
+		for (int i = 1; i < n; i++) {
+			char key = characters[i];
+			int j = i - 1;
+
+			while (j >= 0 && characters[j] > key) {
+				characters[j+1] = characters[j];
+				j--;
+			}
+			characters[j+1] = key;
+		}
+	}
+
 
 }
